ac1ea15c294a0d402ed8560e75391abfb95c4cc6
jxtxfyd <104012624+jxtxfyd@users.noreply.github.com>
2022-04-19 10:04:46 -0400

    Update main.py

--- main.py
+++ main.py
@@ -6,29 +6,23 @@ from random import shuffle
 client = discord.Client()
 inds = list(range(0, 17))
 
-
-
 @client.event
 async def on_ready():
-print(f"We have logged in as {client.user}")
-
-
+  print(f"We have logged in as {client.user}")
 
 @client.event
 async def on_message(message):
-if message.author == client.user:
-return
+  if message.author == client.user:
+    return
 
-if message.content.startswith('$badquote'):
-  shuffle(bad_list)
-  shuffle(inds)
-  await message.channel.send(bad_list[inds[1]])
+  if message.content.startswith('$badquote'):
+    shuffle(bad_list)
+    shuffle(inds)
+    await message.channel.send(bad_list[inds[1]])
 
-if message.content.startswith('$meme'):
-  with open("birb.jpg", "rb") as f:
-  pic = discord.File(f)
-  await message.channel.send(file=pic)
-
-
+  if message.content.startswith('$meme'):
+    with open("birb.jpg", "rb") as f:
+      pic = discord.File(f)
+      await message.channel.send(file=pic)
 
 client.run(os.getenv("TOKEN"))
