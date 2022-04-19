fe1b8ac3ff4898bfe7dfa4b74d7690c5961433f6
A-Banjo <grace@charitycan.com>
2022-04-19 12:25:59 -0400

    hangman game start

---
+++ hangman.py
@@ -0,0 +1,77 @@
+import random
+
+class Hangman:
+    chosen_word = ""
+    guessed_letters = ""
+    remaining_guesses = 6
+
+    words = {
+        1: "Elephant",
+        2: "Dictionary",
+        3: "Hackday",
+        4: "Virtual",
+        5: "Environment",
+        6: "Python",
+        7: "Tuesday",
+        8: "Heroku",
+        9: "Discord",
+        10: "Robot"
+    }
+
+    game_ended = False
+    game_won = False
+
+    def start_game(self):
+        random.seed()
+        key = random.randint(1, len(self.words))
+        self.chosen_word = self.words[key]
+
+        new_string = ""
+        for i in range(0, len(self.chosen_word)):
+            new_string += "?"
+        self.guessed_letters = new_string
+
+    def get_game_status(self):
+        message = ""
+
+        if not self.game_ended:
+            letters = ""
+            for i in range(0, len(self.guessed_letters)):
+                letters += self.guessed_letters[i] + " "
+            message = '{} guesses left \n'.format(self.remaining_guesses)
+            message += letters
+
+        if self.game_ended and self.game_won:
+            message += '\n You won! You correctly guessed ' + '`{}`'.format(self.chosen_word)
+        elif self.game_ended and not self.game_won:
+            message += '\n You lost! The correct word was ' + '`{}`'.format(self.chosen_word)
+
+        return message
+
+    def guess(self, message):
+        args = message.split(' ')
+        guess = ""
+        contains_guess = False
+
+        if len(args) > 1:
+            guess = args[1]
+
+        for i in range(0, len(self.chosen_word)):
+            if guess[0] == self.chosen_word[i]:
+                self.guessed_letters = self.guessed_letters[:i] + guess[0] + self.guessed_letters[i + 1:]
+                contains_guess = True
+        if not contains_guess:
+            self.remaining_guesses -= 1
+
+        unguessed_letters = False
+        for letter in self.guessed_letters:
+            if letter == "?":
+                unguessed_letters = True
+
+        if not unguessed_letters:
+            self.game_ended = True
+            self.game_won = True
+
+        if self.remaining_guesses < 0:
+            self.game_ended = True
+            self.game_won = False 
--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import discord
 import os
+from hangman import Hangman
 
 client = discord.Client()
+game = Hangman()
 
 @client.event
 async def on_ready():
@@ -9,10 +11,22 @@ async def on_ready():
 
 @client.event
 async def on_message(message):
+  args = message.content.split(' ')
   if message.author == client.user:
     return
 
   if message.content.startswith('$'):
     await message.channel.send("hello")
 
-client.run(os.environ("TOKEN"))
+  elif message.content.startswith('!hangman'):
+    game_message = ""
+    if len(args) > 1:
+      if args[1] == 'start':
+        game.start_game()
+        game_message = "A word has been randomly selected (all lowercase). \nGuess letters by using `!hangman x` (x being the guessed letter)."
+
+      else:
+        game.guess(message.content)
+    await message.channel.send(game_message + game.get_game_status())
+    
+client.run("OTY1OTY0MzcwMDA3OTUzNDkw.Yl62bA.UcY5K-jFTyuLhiqo4U9JB35w76I")
